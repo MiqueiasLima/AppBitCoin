c570afd7b199efed1fb4707c36eacec21101390b
MiqueiasLima <miqueiaslima@outlook.com>
2021-06-15 15:41:49 -0300

    Criando Classe do App e importando pacotes http

---
+++ lib/BitCoin.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BitCoin extends StatefulWidget {
+  const BitCoin({Key? key}) : super(key: key);
+
+  @override
+  _BitCoinState createState() => _BitCoinState();
+}
+
+class _BitCoinState extends State<BitCoin> {
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-
+// @dart=2.9
 
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main(){
   runApp(MaterialApp(
