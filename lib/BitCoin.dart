feb634a88d61e255e59ad83cf94ef551637dc66e
MiqueiasLima <miqueiaslima@outlook.com>
2021-06-15 16:20:47 -0300

    Finalizando Layout do App

--- lib/BitCoin.dart
+++ lib/BitCoin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class BitCoin extends StatefulWidget {
   const BitCoin({Key? key}) : super(key: key);
@@ -9,12 +11,36 @@ class BitCoin extends StatefulWidget {
 
 class _BitCoinState extends State<BitCoin> {
 
+  String url = "https://blockchain.info/ticker";
+  double priceBRL = 0;
 
+  _atualizarPreco() async {
 
+    http.Response response = await http.get(url);
+    print(response.body);
+    Map<String,dynamic> preco = json.decode(response.body);
 
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(16),
+            child: Image.asset("imagens/bitcoin.png"),),
+            Padding(padding: EdgeInsets.all(16),
+            child: Text("R")),
+            ElevatedButton(onPressed: _atualizarPreco, child: Text("Atualizar"),style: ElevatedButton.styleFrom(
+              primary: Colors.orange,
+              onPrimary: Colors.white,
+            ),)
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 // @dart=2.9
 
 
+import 'package:appbitcoin/BitCoin.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
 
 void main(){
   runApp(MaterialApp(
-
+      title: "Flutter Aplication",
+      debugShowCheckedModeBanner: false,
+      home: BitCoin(),
   ));
 }
 
